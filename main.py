618e801df9df42138f3da480d09d11308d70a63a
Håkon Anders Strømsodd <haakon8855@gmail.com>
2018-01-18 18:53:10 +0100

    Render
    Lagde funksjonen som rendrer tekst

--- main.py
+++ main.py
@@ -2,32 +2,46 @@ import pygame
 from pygame.locals import *
 
 
+text_on_screen = "Lorem ipsum"
+
+
 class App():
 
 
 	def __init__(self):
-		self._running = True
-		self._display_surf = None
+		self.running = True
+		self.display_surf = None
 		self.size = self.width, self.height = 640, 400
 
 
 	def on_init(self):
 		pygame.init()
-		self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-		self._running = True
+		self.display_surf = pygame.display.set_mode(self.size)
+		self.running = True
+		self.font = pygame.font.SysFont("comicsansms", 16)
 
 
 	def on_event(self, event):
 		if event.type == pygame.QUIT:
-			self._running = False
+			self.running = False
 
 
 	def on_loop(self):
 		pass
 
 
-	def on_render(self):
-		pass
+	def on_render(self, content, centered = False):
+		self.text = self.font.render(content, False, (0, 255, 0))
+
+		if centered == True:
+			self.text_x = self.width // 2 - self.text.get_width() // 2
+			self.text_y = self.height // 2 - self.text.get_height() // 2
+		else:
+			self.text_x = 5
+			self.text_y = 5
+		
+		self.display_surf.blit(self.text, (self.text_x, self.text_y))
+		pygame.display.flip()
 
 
 	def on_cleanup(self):
@@ -36,13 +50,13 @@ class App():
 
 	def on_execute(self):
 		if self.on_init() == False:
-			self._running = False
+			self.running = False
 
-		while self._running:
+		while self.running:
 			for event in pygame.event.get():
 				self.on_event(event)
 			self.on_loop()
-			self.on_render()
+			self.on_render(text_on_screen)
 		self.on_cleanup()
 
 
