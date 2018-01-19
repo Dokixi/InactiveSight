fa2ce2e6e39fbc5675bbe50f655bc8b98ea514ba
Håkon Anders Strømsodd <haakon8855@gmail.com>
2018-01-19 22:07:03 +0100

    Jobber med render og main

--- lib/render.py
+++ lib/render.py
@@ -9,8 +9,8 @@ class Render():
 			self.text = app.font.render(content, False, (0, 255, 0))
 
 			if centered == True:
-				self.text_x = self.width // 2 - self.text.get_width() // 2
-				self.text_y = self.height // 2 - self.text.get_height() // 2
+				self.text_x = app.width // 2 - self.text.get_width() // 2
+				self.text_y = app.height // 2 - self.text.get_height() // 2
 			else:
 				self.text_x = 5
 				self.text_y = 5
--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from pygame.locals import *
 from lib.render import *
 
 
-text_on_screen = "Hello world" #tekst som skal vises på skjermen
+text_on_screen = """
+	Velkommen til Romskjekk på SKAP! \n
+	Dette spillet går ut på at du skal huske oppgavene på romsjekk, \n
+	og ikke minst gjøre dem.\n\n
+
+	Hvis du ikke greier å gjøre alle oppgavene før romsjekken klokken 15:00\n
+	taper du spillet og kan ikke fortsette...\n\n
+
+	Lykke til!
+""" #tekst som skal vises på skjermen
 
 
 class App(): #klassen til vinduet som spillet foregår i
@@ -18,18 +27,21 @@ class App(): #klassen til vinduet som spillet foregår i
 		self.display_surf = pygame.display.set_mode(self.size)
 		pygame.display.set_caption("Test")
 		self.running = True
-		self.font = pygame.font.SysFont("comicsansms", 16)
+		self.font = pygame.font.SysFont("arial", 16)
+		self.clock = pygame.time.Clock()
 
 
 	def on_event(self, event):
 		if event.type == pygame.QUIT:
 			self.running = False
-		if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-			self.running = False
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				self.running = False
 
 
 	def on_loop(self):
-		pass		
+		self.clock.tick(30)
+		renderer.render_text(self, text_on_screen, True)
 
 
 	def on_cleanup(self):
@@ -41,7 +53,6 @@ class App(): #klassen til vinduet som spillet foregår i
 			for event in pygame.event.get():
 				self.on_event(event)
 			self.on_loop()
-			renderer.render_text(self, text_on_screen, False)
 		self.on_cleanup()
 
 
