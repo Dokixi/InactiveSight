c0b90a75a961906b5f6097ad8ff2e544dd1c757d
Håkon Anders Strømsodd <haakon8855@gmail.com>
2018-01-21 21:40:42 +0100

    Kommentarer
    Kommenterte koden litt mer

--- lib/render.py
+++ lib/render.py
@@ -1,55 +1,51 @@
 class Render():
 
 
-	def __init__(self, pygame):
-		self.pygame = pygame
-		self.test_var = 0
+	def __init__(self, pygame): #init-funksjon som kjører når klassen genereres
+		self.pygame = pygame #gjør at vi kan bruke pygame i render.py
 
 
-	def render_text(self, app, content, centered = False):
+	def render_text(self, app, content, color, centered = False):
+		#deler opp strengen content og splitter den hver gang det står "<br>"
+		content_split = content.split("<br>")
 
-			content_split = content.split("<br>")
-			if self.test_var == 0:
-				print(content_split)
-				self.test_var = 1
+		if centered == True: #stemmer hvis teksten er satt til å være midtstilt
+			self.line0 = content_split[0] #innholdet på første linje
 
-			if centered == True:
-				#innholdet på første linje
-				self.line0 = content_split[0]
+			#rendrer teksten først en gang for å finne linjehøyden
+			self.text = app.font.render(self.line0, False, color)
+			self.line_height = self.text.get_height()
 
-				#rendrer teksten først en gang for å finne linjehøyden
-				self.text = app.font.render(self.line0, False, (0, 255, 0))
-				self.line_height = self.text.get_height()
+			#finner så høyden på all teksten som skal vises
+			self.text_height = self.line_height * len(content_split)
 
-				#finner så høyden på all teksten som skal vises
-				self.text_height = self.line_height * len(content_split)
+			#finner x- og y-verdien til første linje og viser den
+			self.line_x = app.width / 2 - self.text.get_width() / 2
+			self.line_y = app.height / 2 - (self.text_height / 2)
+			app.display_surf.blit(self.text, (self.line_x, self.line_y))
 
-				#finner x- og y-verdien til første linje og viser den
-				self.line_x = app.width / 2 - self.text.get_width() / 2
-				self.line_y = app.height / 2 - (self.text_height / 2)
-				app.display_surf.blit(self.text, (self.line_x, self.line_y))
+			#for-løkke som går gjennom hver linje i content_split
+			i = 0
+			for line in content_split:
 
-				#self.text_y = app.height / 2 - self.text.get_height() / 2
-
-				#løkke som går gjennom hver linje
-				i = 0
-				for line in content_split:
-					if i == 0:
-						i += 1
-						continue
-					self.text = app.font.render(line, False, (0, 255, 0))
-					self.line_x = app.width / 2 - self.text.get_width() / 2
-					self.line_y += self.line_height
-					app.display_surf.blit(self.text, (self.line_x, self.line_y))
+				#hopper over første runde i løkken ettersom første linje
+				#allerede er rendret
+				if i == 0:
 					i += 1
+					continue
 
+				self.text = app.font.render(line, False, color)
+				self.line_x = app.width / 2 - self.text.get_width() / 2
+				self.line_y += self.line_height
+				app.display_surf.blit(self.text, (self.line_x, self.line_y))
+				i += 1
 
-			else:
-				self.line_x = 5
-				self.line_y = 5
-				for line in content_split:
-					self.text = app.font.render(line, False, (0, 255, 0))
-					app.display_surf.blit(self.text, (self.line_x, self.line_y))
-					self.line_y += self.text.get_height()
-			
-			self.pygame.display.flip()
+		else:
+			self.line_x = 5
+			self.line_y = 5
+			for line in content_split:
+				self.text = app.font.render(line, False, color)
+				app.display_surf.blit(self.text, (self.line_x, self.line_y))
+				self.line_y += self.text.get_height()
+
+		self.pygame.display.flip()
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
-from pygame.locals import *
 from lib.render import *
 
 
-text_on_screen = """Velkommen til Romskjekk på SKAP!<br>Dette spillet går ut på at du skal huske oppgavene på romsjekk,<br>og ikke minst gjøre dem.<br><br>Hvis du ikke greier å gjøre alle oppgavene før romsjekken klokken 15:00<br>taper du spillet og kan ikke fortsette...<br><br>Lykke til!""" #tekst som skal vises på skjermen
+text_on_screen = "Heisann<br>Hva gjør du nå?<br><br>Vi har det bra!" #tekst som skal vises på skjermen
+colors = {
+	"red": (255, 0, 0),
+	"green": (0, 255, 0),
+	"blue": (0, 0, 255)
+}
 
 
 class App(): #klassen til vinduet som spillet foregår i
@@ -14,32 +18,39 @@ class App(): #klassen til vinduet som spillet foregår i
 		self.display_surf = None
 		self.size = self.width, self.height = 640, 400 #skjermens bredde/høyde
 
-		pygame.init()
+		pygame.init() #initierer pygame slik at det kan brukes
+
+		#lager en overflate hvor man kan rendre ting
 		self.display_surf = pygame.display.set_mode(self.size)
 		pygame.display.set_caption("Test")
-		self.running = True
-		self.font = pygame.font.SysFont("arial", 16)
+		self.running = True #bool som sier om spillet skal kjøre eller ikke
+		self.font = pygame.font.SysFont("arial", 16) #tekstens font
+
+		#starter en klokke som teller oppover.
+		#Brukes bl. a. for å definere framerate
 		self.clock = pygame.time.Clock()
 
 
-	def on_event(self, event):
-		if event.type == pygame.QUIT:
+	def on_event(self, event): #metode som kjører hver gang et event skjer
+		if event.type == pygame.QUIT: #avslutter hvis spillet krysses ut
 			self.running = False
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
+		elif event.type == pygame.KEYDOWN: #kjører hvis en knapp trykkes ned
+			if event.key == pygame.K_ESCAPE: #avslutter spillet når esc trykkes
 				self.running = False
 
 
-	def on_loop(self):
-		self.clock.tick(30)
-		renderer.render_text(self, text_on_screen, False)
+	def on_loop(self): #metode som kjører hver frame
+		self.clock.tick(30) #setter framerate til 30 fps
+		#viser tekst på skjermen
+		renderer.render_text(self, text_on_screen, colors["green"], True)
 
 
-	def on_cleanup(self):
+	def on_cleanup(self): #metode som kjører når programmet avsluttes
 		pygame.quit()
 
 
-	def on_execute(self):
+	def on_execute(self): #metode som kjører når programmet starter
+		#while-løkke som kjører så lenge self.running er True
 		while self.running:
 			for event in pygame.event.get():
 				self.on_event(event)
